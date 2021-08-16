a01ea07af83fbb50e20b8c8c49ee2703148d7d88
abayomi <mac@macs-MacBook-Pro.local>
2021-08-16 19:51:43 +0100

    addedicons&Book

---
+++ src/Book.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export default class Book extends Component {
+    render() {
+        return (
+        
+                
+            
+          <div className="book">
+            <div className="book-top">
+             
+            
+          </div>
+      
+
+
+            </div>
+        )
+    }
+}
